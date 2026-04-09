b07370985d9f479423890a4c0c6d1f4986413b13
Ranilson Baldez <ranilson12@gmail.com>
2026-04-09 13:50:37 -0300

    add cardápios

--- app/cestas.js
+++ app/cestas.js
@@ -1,32 +1,80 @@
 export const cestas = [
   {
     id: 1,
-    nome: "Cesta Amanhecer Premium",
-    preco: "185,00",
+    nome: "Café Bom dia com Amor",
+    preco: "80,00",
     imagem:
       "https://images.unsplash.com/photo-1528698827591-e19ccd7bc23d?q=80&w=500",
     itens: [
-      "Suco de Laranja Natural",
-      "Caneca de Porcelana",
-      "Croissants Amanteigados",
-      "Geleia Artesanal",
-      "Frutas da Estação",
-      "Cartão Personalizado",
+      "Caixa Decorativa",
+      "Cartinha Personalizada",
+      "Arranjo de Balões",
+      "2 Sanduíches de Queijo e Presunto",
+      "Pão de Queijo (60g)",
+      "Maçã Fresca",
+      "Uva",
+      "Kiwi",
+      "Suco Natural (300ml)",
+      "Mini Manteiga",
+      "Sachê de Cappuccino",
     ],
   },
   {
     id: 2,
-    nome: "Cesta Paixão & Vinho",
-    preco: "250,00",
+    nome: "Café Manhã dos Sonhos",
+    preco: "180,00",
     imagem:
-      "https://images.unsplash.com/photo-1559181567-c3190ca9959b?q=80&w=500",
+      "https://images.unsplash.com/photo-1517260911058-0fcfd733702f?q=80&w=500", // Sugestão de imagem para uma cesta bem farta
     itens: [
-      "Vinho Tinto Reservado",
-      "Tábua de Queijos",
-      "Torradas Finas",
-      "Bombons Ferrero",
-      "Cesta de Vime Decorada",
-      "Arranjo de Rosas",
+      "Caixa Decorativa",
+      "Cartinha Personalizada",
+      "Balão Temático",
+      "Pão de Queijo (60g)",
+      "1 Croissant",
+      "2 Pãezinhos com Doce de Leite",
+      "2 Sanduíches de Queijo e Presunto",
+      "Iogurte",
+      "Bolo de Trigo",
+      "Suco Natural (300ml)",
+      "Chocolate Quente (300ml)",
+      "Mini Manteiga",
+      "Sachê de Cappuccino",
+      "Torradas",
+      "Kiwi",
+      "Maçã",
+      "Uvas",
+    ],
+  },
+  {
+    id: 3,
+    nome: "Café Light",
+    preco: "150,00",
+    imagem:
+      "https://images.unsplash.com/photo-1490474418585-ba9bad8fd0ea?q=80&w=500", // Imagem sugerida com frutas e visual leve
+    itens: [
+      "Água de Coco",
+      "Suco Natural (Sem Açúcar)",
+      "Mamão com Chia",
+      "Torradas",
+      "Mini Manteiga",
+      "2 Sanduíches Light",
+      "Iogurte Zero Açúcar",
+      "Requeijão Light",
+      "Uva",
+      "Maçã",
+      "Kiwi",
+      "Sachê de Cappuccino",
     ],
   },
 ];
+
+{
+  /* ADICIONAIS 
+
+Ferreiro Rocher  3 un  8,00
+Suco natural 300ml 7,00
+Croissant   8,00
+Barra de chocolate  12,00
+Chocolate quente 300ml 9,90 
+Pão de queijo 60g  8,00 */
+}
